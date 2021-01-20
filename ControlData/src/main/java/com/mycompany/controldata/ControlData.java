6644977448fcbd755dfb2dfc40e9a33650efee1c
a13yeronbm <a13yeronbm@W10N-I4E18>
2021-01-20 16:17:33 +0100

    Version - Base aportada por Esther

--- ControlData/src/main/java/com/mycompany/controldata/ControlData.java
+++ ControlData/src/main/java/com/mycompany/controldata/ControlData.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
 public class ControlData {
 
     /**
+     * Esto es para borrar
      * Comproba que un parámetro está dentro dun rango
      *
      * @param l1 Tipo int - límite inferior del rango de números
